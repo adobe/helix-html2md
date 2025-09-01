4ca2b966ce42659d4d4f7978b1f84d8b4a6a02c4
Tobias Bocanegra <tripod@adobe.com>
2025-09-01 17:26:36 +0200

    feat: add better image size error

--- test/mdast-process-images.test.js
+++ test/mdast-process-images.test.js
@@ -12,7 +12,23 @@
 
 /* eslint-env mocha */
 import assert from 'assert';
-import { processImages, TooManyImagesError } from '../src/mdast-process-images.js';
+import { processImages, TooManyImagesError, toSISize } from '../src/mdast-process-images.js';
+
+describe('Utils Test', () => {
+  it('calculates the correct si size', () => {
+    assert.strictEqual(toSISize(0), '0B');
+    assert.strictEqual(toSISize(100), '100B');
+    assert.strictEqual(toSISize(-100), '-100B');
+    assert.strictEqual(toSISize(1024), '1.00KB');
+    assert.strictEqual(toSISize(1024 + 512), '1.50KB');
+    assert.strictEqual(toSISize(-1024 - 512), '-1.50KB');
+    assert.strictEqual(toSISize(1024 * 1024), '1.00MB');
+    assert.strictEqual(toSISize(1024 * 1024, 0), '1MB');
+    assert.strictEqual(toSISize(1024 * 1024 * 1024), '1.00GB');
+    assert.strictEqual(toSISize(2048 * 1024 * 1024 * 1024), '2.00TB');
+    assert.strictEqual(toSISize(-2048 * 1024 * 1024 * 1024), '-2.00TB');
+  });
+});
 
 describe('mdast-process-images Tests', () => {
   let processedUrls;
