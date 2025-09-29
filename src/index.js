88cdbad6394b814f9041d521568e4726b2bd870f
Tobias Bocanegra <tripod@adobe.com>
2025-09-29 18:38:10 +0200

    fix: respect maxHTMLSize limit

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import { cleanupHeaderValue } from '@adobe/helix-shared-utils';
 import { MediaHandler, SizeTooLargeException } from '@adobe/helix-mediahandler';
 import pkgJson from './package.cjs';
 import { ConstraintsError, html2md } from './html2md.js';
-import { TooManyImagesError } from './mdast-process-images.js';
+import { TooManyImagesError, toSISize } from './mdast-process-images.js';
 
 /* c8 ignore next 7 */
 export const { fetch } = h1NoCache();
@@ -32,6 +32,8 @@ export const { fetch } = h1NoCache();
 const gzip = promisify(zlib.gzip);
 
 const DEFAULT_MAX_IMAGE_SIZE = 20 * 1024 * 1024; // 20mb
+
+const DEFAULT_MAX_HTML_SIZE = 1024 * 1024; // 1mb
 
 /**
  * Generates an error response
@@ -153,9 +155,13 @@ async function run(request, ctx) {
         return error(ctx, `error fetching resource at ${sourceUrl}: ${status}`, 502, 'warn');
       }
     }
+    const maxHTMLSize = ctx.data.limits?.maxHTMLSize
+      ? parseInt(ctx.data.limits.maxHTMLSize, 10)
+      : DEFAULT_MAX_HTML_SIZE;
+
     // limit response size of content provider to 1mb
-    if (html.length > 1024 * 1024) {
-      return error(ctx, `error fetching resource at ${sourceUrl}: html source larger than 1mb`, 409);
+    if (html.length > maxHTMLSize) {
+      return error(ctx, `error fetching resource at ${sourceUrl}: html source larger than ${toSISize(maxHTMLSize, 0)}`, 409);
     }
   } catch (e) {
     if (e instanceof AbortError) {
